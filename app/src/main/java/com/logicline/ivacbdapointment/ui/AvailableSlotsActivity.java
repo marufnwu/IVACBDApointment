d78acda6dcd9908e9e51d6cd23f6193b4268e000
antorsaha <antor145648s@gmail.com>
2023-11-09 19:14:45 +0600

    working on available time slots

---
+++ app/src/main/java/com/logicline/ivacbdapointment/ui/AvailableSlotsActivity.java
@@ -0,0 +1,16 @@
+package com.logicline.ivacbdapointment.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.logicline.ivacbdapointment.R;
+
+public class AvailableSlotsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_avaliable_slots);
+    }
+}
